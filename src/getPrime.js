ad03b766a9fe5dc73cc0b2bb6d356bad97983c84
ronkeadun <ronkeadun1@yahoo.com>
2017-07-03 21:54:51 +0100

    initial commit

---
+++ src/getPrime.js
@@ -0,0 +1,23 @@
+const getPrimes=(n)=> {
+
+    var num = [], primes = [];
+
+    for (var i = 2; i <= n; ++i) {
+
+        if (!num[i]) {
+
+            primes.push(i);
+
+            for (var j = i << 1; j <= n; j += i) {
+
+                num[j] = true;
+
+            }
+
+        }
+
+    }
+
+    return primes;
+
+}
---
+++ test/test.js
@@ -0,0 +1,42 @@
+const assert = require('chai').assert;
+const myApp = require('..src/getPrime.js')
+
+describe("Get prime numbers", ()=> {
+
+  it("should return [] as prime numbers for 0", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(0), []);
+
+  })
+
+  it("should return [ 2, 3, 5] as prime numbers for 25", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(5), [ 2, 3, 5]);
+
+  })
+
+  it("should return [2, 3, 5, 7, 11, 13] as prime numbers for 17", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(17), [ 2, 3, 5, 7, 11, 13 ]);
+
+  })
+
+  it("should return [2, 3, 5, 7] as prime numbers for 8", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(8), [ 2, 3, 5, 7]);
+
+  })
+
+  it("should return [] as prime numbers for 1", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(1), []);
+
+  })
+
+  it("should return [2] as prime numbers for 2", ()=> {
+
+    assert.deepEqual(myApp.getPrimes(-20), []);
+
+  })
+
+})
